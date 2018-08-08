f3854ecf08a6a95d7210fbcc724bc9400328d45f
Reece_Collett <rcollett@spartaglobal.com>
2018-08-08 16:40:31 +0100

    Added subtraction test

--- js_AdvCalculator_hw/spec/advCalcMethodsSpec.js
+++ js_AdvCalculator_hw/spec/advCalcMethodsSpec.js
@@ -21,4 +21,41 @@ describe("This is testing my advanced calculator", function()
     expect(calculator.Addition(1.44, 2.4)).toEqual(3.84);
   })
 
-  
+  // Subtraction
+  it("should subtract 2 numbers", function() {
+    expect(calculator.Subtract(3, 2)).toEqual(1);
+    expect(calculator.Subtract(6, 3)).toEqual(3);
+    expect(calculator.Subtract(1, 3)).toEqual(-2);
+  })
+
+  it("should subtract 2 negative numbers", function() {
+    expect(calculator.Subtract(-1, 2)).toEqual(-3);
+    expect(calculator.Subtract(-2, -9)).toEqual(7);
+    expect(calculator.Subtract(-1, 3)).toEqual(-4);
+  })
+
+  it("should subtract 2 decimal numbers", function() {
+    expect(calculator.Subtract(1.5, 0.5)).toEqual(1);
+    expect(calculator.Subtract(1, 0.4)).toEqual(0.6);
+    expect(calculator.Subtract(6.25, 3.25)).toEqual(3);
+  })
+
+  // Multiply
+  it("should multiply 2 numbers", function() {
+    expect(calculator.Multiply(3, 2)).toEqual(6);
+    expect(calculator.Multiply(6, 3)).toEqual(18);
+    expect(calculator.Multiply(1, 3)).toEqual(3);
+  })
+
+  it("should multiply 2 negative numbers", function() {
+    expect(calculator.Multiply(-1, 2)).toEqual(-2);
+    expect(calculator.Multiply(-2, -9)).toEqual(18);
+    expect(calculator.Multiply(-1, 3)).toEqual(-3);
+  })
+
+  it("should multiply 2 decimal numbers", function() {
+    expect(calculator.Multiply(1.5, 0.5)).toEqual(0.75);
+    expect(calculator.Multiply(1, 0.4)).toEqual(0.4);
+    expect(calculator.Multiply(6.25, 3.25)).toEqual(20.3125);
+  })
+})
